78110de7e8cdffbdc4967fb4dd3ddc14ce505fd7
Avital Rodov <avital.rodov@gmail.com>
2020-11-16 17:39:06 +0200

    creating store page

---
+++ lib/Screens/store.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StoreScreen extends StatelessWidget {
+  StoreScreen();
+  static const routhName = "/store";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
