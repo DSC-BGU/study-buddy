1c49148e8a20321376f7e3aa0f398e38ef632dbc
Shira Segev <shirasegev@gmail.com>
2020-11-16 17:37:41 +0200

    creating the mall page

---
+++ lib/Screens/mall.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MallScreen extends StatelessWidget {
+  MallScreen();
+  static const routhName = '/mall';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
