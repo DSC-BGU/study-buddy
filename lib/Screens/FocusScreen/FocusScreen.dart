446ae5c153c1ba36255673e55bd0ae837d61cd39
Roei Bracha <roei635@gmail.com>
2020-11-30 13:10:36 +0200

    fix types to duration

--- lib/Screens/FocusScreen/FocusScreen.dart
+++ lib/Screens/FocusScreen/FocusScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_circular_slider/flutter_circular_slider.dart';
 import 'package:study_buddy/widgets/MainScreen/PointsStatus.dart';
 import 'package:study_buddy/widgets/MainScreen/StartButton.dart';
+import '../../utils/TimeFunctions.dart';
 
 class FocusScreen extends StatelessWidget {
   const FocusScreen({Key key}) : super(key: key);
@@ -48,29 +49,33 @@ class FocusCircleSlider extends StatefulWidget {
 }
 
 class _FocusCircleSliderState extends State<FocusCircleSlider> {
-  int _currentState;
+  Duration _currentDuration;
 
   @override
   void initState() {
-    _currentState = this.widget.defaultStart;
+    _currentDuration =
+        _currentDuration = Duration(minutes: this.widget.defaultStart);
     super.initState();
   }
 
   void onTimeChange(int init, int end, int laps) {
     setState(() {
-      _currentState = end;
+      _currentDuration = Duration(minutes: end);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final setTime = printDuration(_currentDuration);
     return Container(
       child: SingleCircularSlider(
         this.widget.maxMinutes,
-        _currentState,
+        _currentDuration.inMinutes,
         baseColor: Theme.of(context).accentColor,
         handlerColor: Theme.of(context).primaryColor,
-        child: Center(child: Text(_currentState.toString())),
+        child: Center(
+          child: Text(setTime),
+        ),
         onSelectionChange: onTimeChange,
       ),
     );
