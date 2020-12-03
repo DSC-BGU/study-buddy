8075236c519ad263f6ad0cb096cf170a05e85bcc
Omri Elmaliach <omrielma@post.bgu.ac.il>
2020-12-03 14:13:10 +0200

    Cupons List ready with pop QR

--- lib/Screens/Cupons.dart
+++ lib/Screens/Cupons.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:study_buddy/widgets/SingleCupon.dart';
 
 class Cupons extends StatelessWidget {
+  final List<SingleCupon> cuponsList = [
+    SingleCupon('Hamburger', '1+1 on all Hamburgers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers'),
+    SingleCupon('Beer', '20% of all beers')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,15 +21,18 @@ class Cupons extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         title: Text("My Cupons"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(20),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Text(
-              'My Cupons',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 10, 0, 8),
+              child: Text(
+                'My Cupons',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
-            SingleCupon()
+            SingleCupon('Hamburger', '1+1 on all Hamburgers'),
+            SingleCupon('Beer', '20% of all beers'),
           ],
         ),
       ),
---
+++ lib/Screens/PopUpQR.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class PopUpQR extends StatelessWidget {
+  final BuildContext ctx;
+  final String cuponTitle;
+  PopUpQR(this.ctx, this.cuponTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      backgroundColor: Colors.black45,
+      insetPadding: EdgeInsets.fromLTRB(0, 100, 0, 120),
+      title: Center(
+          child: Text(
+        'Present This Code',
+        style: TextStyle(color: Colors.white),
+      )),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text('$cuponTitle',
+              style: TextStyle(color: Colors.white, fontSize: 24)),
+          Container(
+            height: 170,
+            width: 170,
+            child: Card(
+              child: Center(
+                child: Text(
+                  'QR',
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 45,
+            width: 200,
+            child: TextButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)))),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    backgroundColor: MaterialStateProperty.all(Colors.grey)),
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  'Got it, Thanks',
+                  style: TextStyle(fontSize: 19),
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
