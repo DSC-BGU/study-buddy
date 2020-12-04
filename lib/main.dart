2a131670dbf5106cce11783d2cd0850611a5ef78
Roei Bracha <roei635@gmail.com>
2020-12-05 00:57:19 +0200

    adding points provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:study_buddy/Screens/FocusScreen/FocusScreen.dart';
+import 'package:study_buddy/providers/points.dart';
 import './Screens/MainScreen/MainScreen.dart';
 import 'package:study_buddy/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -12,40 +14,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('he', 'IL'),
-      ],
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      localeResolutionCallback: (locale, supportedLocales) {
-        for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode &&
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
-          }
-        }
-        return supportedLocales.first;
-      },
-      routes: {
-        '/': (ctx) => MainScreen(),
-        FocusScreen.routeName: (ctx) => FocusScreen()
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == FocusScreen.routeName)
-          return PageRouteBuilder(pageBuilder: (_, __, ___) => FocusScreen());
-        return null;
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => Points()),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          supportedLocales: [
+            Locale('en', 'US'),
+            Locale('he', 'IL'),
+          ],
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          localeResolutionCallback: (locale, supportedLocales) {
+            for (var supportedLocale in supportedLocales) {
+              if (supportedLocale.languageCode == locale.languageCode &&
+                  supportedLocale.countryCode == locale.countryCode) {
+                return supportedLocale;
+              }
+            }
+            return supportedLocales.first;
+          },
+          routes: {
+            '/': (ctx) => MainScreen(),
+            FocusScreen.routeName: (ctx) => FocusScreen(),
+          },
+        ));
   }
 }
