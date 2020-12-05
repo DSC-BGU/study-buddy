b4e28589965763517dd29508544251e680d0d344
Roei Bracha <roei635@gmail.com>
2020-12-05 12:24:05 +0200

    adding option to navigate to spacific tab

--- lib/Screens/TabsScreen.dart
+++ lib/Screens/TabsScreen.dart
@@ -6,12 +6,24 @@ import 'package:study_buddy/Screens/store.dart';
 
 import '../app_localizations.dart';
 
+class TabsScreenArguments {
+  int selectedTab;
+  TabsScreenArguments({
+    this.selectedTab,
+  });
+}
+
 class TabsScreen extends StatelessWidget {
+  static const String routeName = '/';
   @override
   Widget build(BuildContext context) {
     String t(String text) => AppLocalizations.of(context).translate(text);
+    final TabsScreenArguments args = ModalRoute.of(context).settings.arguments;
+    int _intialIndex = args?.selectedTab != null ? args.selectedTab : 1;
+
     return DefaultTabController(
       length: 3,
+      initialIndex: _intialIndex,
       child: Scaffold(
         appBar: AppBar(
           title: Text(t("Hi user")),
@@ -22,6 +34,7 @@ class TabsScreen extends StatelessWidget {
         bottomNavigationBar: Container(
           color: Theme.of(context).primaryColor,
           child: TabBar(
+            // controller: _tabController,
             tabs: [
               Tab(
                 icon: Icon(MaterialCommunityIcons.store),
@@ -44,3 +57,9 @@ class TabsScreen extends StatelessWidget {
     );
   }
 }
+
+// void navigateExample = (){
+//   Navigator.pushNamed(context, TabsScreen.routeName,
+//         arguments: TabsScreenArguments(
+//             selectedTab: 1));
+// }
