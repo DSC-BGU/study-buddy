47910a655404f1359f5b7fbde68b411515babf83
Omri Elmaliach <omrielma@post.bgu.ac.il>
2020-12-07 16:16:33 +0200

    Fixed errors for coupons

--- lib/widgets/SingleCoupon.dart
+++ lib/widgets/SingleCoupon.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:study_buddy/app_localizations.dart';
 
 class SingleCoupon extends StatelessWidget {
   final String id;
@@ -37,70 +36,26 @@ class SingleCoupon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String t(String text) => AppLocalizations.of(context).translate(text);
-    return Container(
-      height: 140,
-      child: Stack(children: [
-        Image.network(
-          imageUrl,
-          width: double.infinity,
-          fit: BoxFit.fitWidth,
-        ),
-        Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          Container(
-              width: double.infinity,
-              color: Colors.black38,
-              child: Text(
-                this.description,
-                style: TextStyle(color: Colors.white, fontSize: 26),
-              )),
-        ])
-      ]),
-      // child: Column (
-      //   children: [
-      //     Stack(
-      //       children: [
-      //         ClipRRect(
-      //           borderRadius: BorderRadius.only(
-      //             topLeft: Radius.circular(15),
-      //             topRight: Radius.circular(15),
-      //           ),
-      //           child: Image.network(
-      //             imageUrl,
-      //             height: 250,
-      //             width: double.infinity,
-      //             fit: BoxFit.fill,
-      //           ),
-      //         ),
-      //         Positioned(
-      //           bottom: 20,
-      //           right: 10,
-      //           child: Container(
-      //             width: 300,
-      //             color: Colors.black54,
-      //             padding: EdgeInsets.symmetric(
-      //               vertical: 5,
-      //               horizontal: 20,
-      //             ),
-      //             child: Text(
-      //               title,
-      //               style: TextStyle(
-      //                 fontSize: 26,
-      //                 color: Colors.white,
-      //               ),
-      //               softWrap: true,
-      //               overflow: TextOverflow.fade,
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //       children: [],
-      //     ),
-      //   ],
-      // ),
-    );
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Container(
+        height: constraints.maxWidth * 0.30,
+        child: Stack(children: [
+          Image.network(
+            imageUrl,
+            width: double.infinity,
+            fit: BoxFit.fitWidth,
+          ),
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Container(
+                width: double.infinity,
+                color: Colors.black38,
+                child: Text(
+                  this.description,
+                  style: TextStyle(color: Colors.white, fontSize: 26),
+                )),
+          ])
+        ]),
+      );
+    });
   }
 }
