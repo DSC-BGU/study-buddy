2b336a63fec07950d5f55c6fa19cb1406600ddb3
Shira Segev <shirasegev@gmail.com>
2021-03-06 17:18:46 +0200

    start

--- lib/Screens/studentScreens/categories_screen.dart
+++ lib/Screens/studentScreens/categories_screen.dart
@@ -1,63 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:provider/provider.dart';
+import 'package:study_buddy/providers/studentProviders/user_provider.dart';
+import 'package:study_buddy/widgets/designs/Button.dart';
+import 'package:study_buddy/widgets/designs/TopCurve.dart';
+import '../../app_localizations.dart';
 import '../../models/sharedModels/Category.dart' as Cat;
 import '../../providers/sharedProviders/StoreProvider.dart';
 import '../../widgets/studentWidgets/CategoryScreen/advertising.dart';
 import '../../widgets/studentWidgets/CategoryScreen/category_item.dart';
+import 'DrawerButton.dart';
+import 'MyCoupons.dart';
 
 class CategoriesScreen extends StatelessWidget {
   static const routeName = '/categories';
 
   @override
   Widget build(BuildContext context) {
+    String t(String text) => AppLocalizations.of(context).translate(text);
+    UserProvider user = Provider.of<UserProvider>(context);
     StoreProvider storeProvider = Provider.of<StoreProvider>(context);
     List<Cat.Category> categoriesList = storeProvider.categories;
     return Scaffold(
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverGrid(
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200.0,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                  childAspectRatio: 3 / 2,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (ctx, constraints) {
+            return Stack(
+              children: [
+                Container(color: Colors.white),
+                TopCurve(),
+                DrawerButton(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: constraints.maxHeight * 0.03,
+                          bottom: constraints.maxHeight * 0.03),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: constraints.maxHeight * 0.07,
+                              backgroundImage:
+                                  AssetImage("assets/avatar_temp.jpg"),
+                            ),
+                            Text(
+                              t("Hii") + " " + user.name,
+                              style: TextStyle(fontSize: 34),
+                            ),
+                            Text("You have" +
+                                " " +
+                                user.points.toString() +
+                                " " +
+                                "points"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Flexible(
+                              child: GridView(
+                                shrinkWrap: true,
+                                padding: const EdgeInsets.all(15),
+                                children: categoriesList
+                                    .map((c) => CategoryItem(c))
+                                    .toList(),
+                                gridDelegate:
+                                    SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: 160,
+                                  childAspectRatio: 3 / 2,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 15),
+                            child: Container(
+                              child: Button(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed(MyCoupons.routeName);
+                                },
+                                child: Text(t("My coupons")),
+                                icon: Icon(Ionicons.md_pricetag),
+                                width: constraints.maxWidth * 0.7,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return Advertising();
-                  },
-                  childCount: 1,
-                ),
-              ),
-              // GridView(
-              //   padding: const EdgeInsets.all(15),
-              //   children: categoriesList.map((c) => CategoryItem(c)).toList(),
-              //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              //     maxCrossAxisExtent: 200,
-              //     childAspectRatio: 3 / 2,
-              //     crossAxisSpacing: 10,
-              //     mainAxisSpacing: 10,
-              //   ),
-              // ),
-              SliverGrid(
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200.0,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                  childAspectRatio: 3 / 2,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return CategoryItem(categoriesList[index]);
-                  },
-                  childCount: categoriesList.length,
-                ),
-              ),
-            ],
-          ),
+              ],
+            );
+          },
         ),
       ),
     );
--- lib/widgets/studentWidgets/CategoryScreen/category_item.dart
+++ lib/widgets/studentWidgets/CategoryScreen/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../app_localizations.dart';
 import '../../../models/sharedModels/Category.dart' as Cat;
 import '../../../Screens/studentScreens/category_stores_screen.dart';
 
@@ -16,31 +17,56 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => selectCategory(context),
-      splashColor: Theme.of(context).primaryColor,
-      // borderRadius: BorderRadius.circular(10),
-      child: Container(
-        child: Center(
-          child: Text(
-            this.category.title,
-            style: Theme.of(context).textTheme.title,
-          ),
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).primaryColor.withOpacity(0.8),
-              Theme.of(context).primaryColor.withOpacity(0.7),
-              // color.withOpacity(0.7),
-              // color,
+    String t(String text) => AppLocalizations.of(context).translate(text);
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return FlatButton(
+          onPressed: () => selectCategory(context),
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  this.category.title,
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ),
             ],
-            // begin: Alignment.center,
-            // end: Alignment.center,
           ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
+          minWidth: constraints.maxWidth * 0.4,
+          color: Theme.of(context).accentColor,
+          textColor: Colors.black,
+          height: constraints.maxHeight * 0.11,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        );
+      },
     );
+
+    // InkWell(
+    //   onTap: () => selectCategory(context),
+    //   splashColor: Theme.of(context).primaryColor,
+    //   // borderRadius: BorderRadius.circular(10),
+    //   child: Container(
+    //     child: Center(
+    //       child: Text(
+    //         this.category.title,
+    //         style: Theme.of(context).textTheme.title,
+    //       ),
+    //     ),
+    //     decoration: BoxDecoration(
+    //       gradient: LinearGradient(
+    //         colors: [
+    //           Theme.of(context).primaryColor.withOpacity(0.8),
+    //           Theme.of(context).primaryColor.withOpacity(0.7),
+    //           // color.withOpacity(0.7),
+    //           // color,
+    //         ],
+    //         // begin: Alignment.center,
+    //         // end: Alignment.center,
+    //       ),
+    //       borderRadius: BorderRadius.circular(10),
+    //     ),
+    //   ),
+    // );
   }
 }
