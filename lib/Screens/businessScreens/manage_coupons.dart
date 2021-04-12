7622fee28a614ce9afa9ff6f7403f38a28f34335
omrielma <omrielma@post.bgu.ac.il>
2021-04-12 17:36:57 +0300

    extra

--- lib/Screens/businessScreens/manage_coupons.dart
+++ lib/Screens/businessScreens/manage_coupons.dart
@@ -23,6 +23,7 @@ class ManageCoupons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String t(String text) => AppLocalizations.of(context).translate(text);
     UserProvider user = Provider.of<UserProvider>(context);
     String storeId = user.storeId;
     List<Coupon> bussinessCoupons = user.getBussinessCoupons(storeId);
@@ -86,7 +87,7 @@ class ManageCoupons extends StatelessWidget {
                             width: constraints.maxWidth * 0.9,
                             child: Center(
                               child: Text(
-                                'ניהול קופונים',
+                                t('ניהול קופונים'),
                                 style: TextStyle(fontSize: 35),
                               ),
                             )),
@@ -128,7 +129,7 @@ class ManageCoupons extends StatelessWidget {
                               },
                               child: Center(
                                 child: Text(
-                                  'חזרה',
+                                  t('חזרה'),
                                   style: TextStyle(
                                     color: Colors.black,
                                     fontSize: 15,
@@ -154,7 +155,7 @@ class ManageCoupons extends StatelessWidget {
                               },
                               child: Center(
                                 child: Text(
-                                  'יצירת\nקופון',
+                                  t('יצירת\nקופון'),
                                   style: TextStyle(
                                     color: Colors.black,
                                     fontSize: 15,
@@ -174,112 +175,5 @@ class ManageCoupons extends StatelessWidget {
         ),
       ),
     );
-    // return LayoutBuilder(builder: (ctx, constraints) {
-    //   return Scaffold(
-    //       backgroundColor: Theme.of(context).backgroundColor,
-    //       appBar: AppBar(
-    //         backgroundColor: Theme.of(context).backgroundColor,
-    //         elevation: 0.0,
-    //         centerTitle: true,
-    //         leading: Icon(
-    //           Icons.face,
-    //           size: 45,
-    //         ),
-    //         actions: [
-    //           Container(
-    //             width: 60,
-    //             decoration: BoxDecoration(
-    //                 color: Theme.of(context).accentColor,
-    //                 borderRadius:
-    //                     BorderRadius.only(bottomLeft: Radius.circular(20))),
-    //             child: IconButton(
-    //                 icon: Icon(
-    //                   Icons.menu,
-    //                   color: Colors.black,
-    //                 ),
-    //                 onPressed: null),
-    //           )
-    //         ],
-    //       ),
-    //       body: Column(
-    //         children: [
-    //           Stack(children: [
-    //             Card(
-    //               color: Theme.of(context).hintColor,
-    //               shape: RoundedRectangleBorder(
-    //                 borderRadius: BorderRadius.circular(15.0),
-    //               ),
-    //               margin: EdgeInsets.fromLTRB(25, 8, 25, 8),
-    //               child: Container(
-    //                   height: constraints.maxHeight * 0.1,
-    //                   width: double.infinity,
-    //                   margin: EdgeInsets.all(8),
-    //                   child: Center(
-    //                     child: Text(
-    //                       'ניהול קופונים',
-    //                       style: TextStyle(
-    //                           color: Theme.of(context).accentColor,
-    //                           fontSize: 35),
-    //                     ),
-    //                   )),
-    //             ),
-    //             Center(
-    //                 child: Icon(Icons.face_rounded,
-    //                     size: 80, color: Theme.of(context).accentColor))
-    //           ]),
-    // Expanded(
-    //   child: ListView.builder(
-    //     itemBuilder: (ctx, index) {
-    //       return BusinessCoupon(coupon: availableCoupons[index]);
-    //     },
-    //     itemCount: availableCoupons.length,
-    //   ),
-    // ),
-    // Row(
-    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //   children: [
-    //     Container(
-    //       margin: EdgeInsets.all(8),
-    //       child: FlatButton(
-    //         height: constraints.maxHeight * 0.10,
-    //         color: Theme.of(context).hintColor,
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(15.0),
-    //         ),
-    //         onPressed: () {},
-    //         child: Center(
-    //           child: Text(
-    //             'חזרה',
-    //             style: TextStyle(
-    //                 fontSize: 15,
-    //                 color: Theme.of(context).accentColor),
-    //           ),
-    //         ),
-    //       ),
-    //     ),
-    //     Container(
-    //       margin: EdgeInsets.all(8),
-    //       child: FlatButton(
-    //         height: constraints.maxHeight * 0.10,
-    //         color: Theme.of(context).hintColor,
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(15.0),
-    //         ),
-    //         onPressed: () {},
-    //         child: Center(
-    //           child: Text(
-    //             'יצירת קופון',
-    //             style: TextStyle(
-    //                 fontSize: 15,
-    //                 color: Theme.of(context).accentColor),
-    //           ),
-    //         ),
-    //       ),
-    //     )
-    //   ],
-    // )
-    //           ],
-    //         ));
-    //   });
   }
 }
